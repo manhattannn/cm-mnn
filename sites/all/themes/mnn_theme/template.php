6f14b0f69c21d0a649631a8455f463153aacf44f
ftm-bill <bill@firstturnmedia.com>
2019-05-06 17:03:44 -0400

    commented out tpl suggestion debug

--- sites/all/themes/mnn_theme/template.php
+++ sites/all/themes/mnn_theme/template.php
@@ -27,8 +27,9 @@ function mnn_theme_links($variables) {
  * Implements template_preprocess_page().
  */
 function mnn_theme_preprocess_page(&$vars) {
-  $debug = $vars['theme_hook_suggestions'];
-  print_r($debug);
+  // @todo remove this when done 
+  //$debug = $vars['theme_hook_suggestions'];
+  //print_r($debug);
 
   $vars['user_menu'] =  theme('links', array('links' => menu_navigation_links('user-menu'), 'attributes' => array('class '=> array('links', 'site-menu'))));
 }
