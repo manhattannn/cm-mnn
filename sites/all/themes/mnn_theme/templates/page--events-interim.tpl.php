86ae5e0904acb078ed62397632478b119548bcc8
ftm-bill <bill@firstturnmedia.com>
2019-05-07 12:35:38 -0400

    added user login form to events interim pg

--- sites/all/themes/mnn_theme/templates/page--events-interim.tpl.php
+++ sites/all/themes/mnn_theme/templates/page--events-interim.tpl.php
@@ -3,5 +3,11 @@
 
 page--events--interim.tpl.php
 
+<?php
+  $user_login_form = drupal_get_form('user_login');
+  $user_login_form_render = drupal_render($user_login_form);
+  print $user_login_form_render;
+?>
+
 <!-- Footer -->
 <?php include 'components/footer.tpl.php';?>
