2b3b59cc2b092288793691e7109e34d36f0e3026
ftm-bill <bill@firstturnmedia.com>
2019-05-07 15:07:06 -0400

    testing adding event title to interim pg

--- sites/all/themes/mnn_theme/templates/page--events-interim.tpl.php
+++ sites/all/themes/mnn_theme/templates/page--events-interim.tpl.php
@@ -5,6 +5,9 @@
   Please login to continue.
 </p>
 
+Event Title:
+<?php print $event_title; ?>
+
 <?php
   $user_login_form = drupal_get_form('user_login');
   $user_login_form_render = drupal_render($user_login_form);
