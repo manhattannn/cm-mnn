3bd6dcfdc6f19d28a5d8c39929382674dade86df
George Boobyer <george.boobyer@gmail.com>
2019-09-27 17:43:57 +0100

    Fix the superfish menu so the parent has no link

--- sites/all/themes/mnn_theme/js/app.js
+++ sites/all/themes/mnn_theme/js/app.js
@@ -1,9 +1,9 @@
 /**
  * @file
- * A JavaScript file for the theme.
  *
- * In order for this JavaScript to be loaded on pages, see the instructions in
- * the README.txt next to this file.
+ * Remove the link from the parent in the superfish menu
+ * Replace it with the effect of opening the menu
+ *
  */
 
 (function ($, Drupal, window, document, undefined) {
@@ -17,5 +17,49 @@
 
     }
   };
+
+  $(window).resize(function(){
+
+    timer = setTimeout(function(){
+      
+        $('#superfish-1-accordion > li > a.menuparent').on('click', function(event) {
+
+            // Making sure the buttons does not exist already.
+            if ($(this).closest('li').children('ul').length > 0) {
+              event.preventDefault();
+              // Selecting the parent menu items.
+              var parent = $(this).closest('li');
+              // Creating and inserting Expand\Collapse buttons to the parent menu items,
+              // of course only if not already happened.
+
+              // Once the button is clicked, collapse the sub-menu if it's expanded.
+              if (parent.hasClass('sf-expanded')) {
+                parent.children('ul').slideUp('fast', function() {
+                  // Doing the accessibility trick after hiding the sub-menu.
+                  $(this).closest('li').removeClass('sf-expanded').end().addClass('sf-hidden').show();
+                });
+
+              }
+              // Otherwise, expand the sub-menu.
+              else {
+                // Doing the accessibility trick and then showing the sub-menu.
+                parent.children('ul').hide().removeClass('sf-hidden').slideDown('fast')
+                  // Changing the caption of the inserted Expand link to 'Collape', if any is inserted.
+                  .end().addClass('sf-expanded').children('a.sf-accordion-button')
+                  // Hiding any expanded sub-menu of the same level.
+                  .end().siblings('li.sf-expanded').children('ul')
+                  .slideUp('fast', function() {
+                    // Doing the accessibility trick after hiding it.
+                    $(this).closest('li').removeClass('sf-expanded').end().addClass('sf-hidden').show();
+                  })
+                  // Assuming Expand\Collapse buttons do exist, resetting captions, in those hidden sub-menus.
+                  .parent().children('a.sf-accordion-button');
+              }
+            }
+
+        });
+
+    }, 75);
+  });
 
 })(jQuery, Drupal, this, this.document);
