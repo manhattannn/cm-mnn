9753df44380da8dd2bab375051fba1bad485caa9
Eric Goldhagen <eric@openflows.com>
2019-11-19 16:40:45 +0000

    bug fix updates for the following modules captcha civicrm_cron date diff editablefields features field_collection options_element viewfield views_autocomplete_filters views_data_export webform webform_civicrm

--- sites/all/modules/contrib/editablefields/editablefields.js
+++ sites/all/modules/contrib/editablefields/editablefields.js
@@ -1,17 +1,35 @@
 (function ($) {
 Drupal.behaviors.editablefields_submit = {
   attach: function (context) {
-    $('.editablefield-item').once('editablefield', function() {
+    $('.editablefield-item', context).once('editablefield', function() {
       var $this = $(this);
 
       // There is only one editable field in that form, we can hide the submit
       // button.
-      if ($this.find('input[type=text],textarea,select').length == 1) {
-        $this.find('input.form-submit').hide();
-        $this.find('input[type=text],textarea,select').change(function() {
-          $this.find('input.form-submit').triggerHandler('click');
+      var $singleEditableFields = $this.find('input[type=text],input[type=checkbox],textarea,select');
+      // Filter out select's from table drag.
+      $singleEditableFields = $singleEditableFields.not('[name*="[_weight]"]');
+      if ($singleEditableFields.length === 1 || $this.find('input[type=radio]').length > 1) {
+        $this.find('.form-submit').hide();
+        $this.find('input[type=text],input[type=checkbox],input[type=radio],textarea,select').change(function () {
+          $this.find('.form-submit').triggerHandler('click');
         });
       }
+
+      var submitName = '.form-submit.editablefield-edit-hover';
+      var linkName = '.editablefield-hover-link';
+
+      var $submit = $this.find(submitName);
+      $submit.hide().after('<a href="#" class="editablefield-hover-link">' + $submit.attr('value') + '</a>');
+
+      $this.find(linkName).hide().click(function () {
+        $this.find(submitName).click();
+        return false;
+      });
+
+      $this.hover(function () {
+        $this.find(linkName).fadeToggle('fast');
+      });
     });
   }
 };
@@ -22,7 +40,7 @@ Drupal.behaviors.editablefields_submit = {
  */
 Drupal.jsAC.prototype.hidePopup = function (keycode) {
   // Select item if the right key or mousebutton was pressed.
-  if (this.selected && ((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
+  if (this.selected && ((keycode && keycode !== 46 && keycode !== 8 && keycode !== 27) || !keycode)) {
     this.input.value = $(this.selected).data('autocompleteValue');
     $(this.input).trigger('change');
   }
