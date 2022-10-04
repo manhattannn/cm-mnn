df6a86bfd4e93cd6195f2828cc3ec138d453f7d0
Eric Goldhagen <eric@openflows.com>
2022-10-04 21:14:21 +0000

    commiting hotfixes for encompass

---
+++ sites/all/modules/contrib/views_data_export/theme/views-data-export-xls-body--encompass-exports.tpl.php
@@ -0,0 +1,5 @@
+
+<?php
+print "test";
+print $view->display_handler->render_area('footer');
+print $tbody; ?>
--- sites/all/modules/contrib/views_data_export/theme/views-data-export-xls-body.tpl.php
+++ sites/all/modules/contrib/views_data_export/theme/views-data-export-xls-body.tpl.php
@@ -1,1 +1,10 @@
-<?php print $tbody; ?>
+<?php
+if($view->name == "encompass_exports") {
+    //        dsm($view);
+        print $view->result[0]->field_field_airing_channel[0]['rendered']['#title']."\n\r<br>";
+        $month_name = $month_name = date("M", mktime(0, 0, 0, $view->exposed_input['field_airing_date_value']['value']['month'], 10));
+               print $view->exposed_input['field_airing_date_value']['value']['day']."-".$month_name."-".$view->exposed_input['field_airing_date_value']['value']['year'];
+                                                                    
+
+    }
+print $tbody; ?>
